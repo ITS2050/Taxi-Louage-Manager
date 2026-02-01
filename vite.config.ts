4571efd7be3abfbaaa8f537b8d8b96ba8de091f5
ITS2050 <infotecservice2050@gmail.com>
2026-02-01 21:56:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Crucial pour GitHub Pages : assure que les assets sont cherchés au bon endroit
   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    cssCodeSplit: false, // Regroupe le CSS pour éviter les erreurs de chargement réseau
+    cssCodeSplit: false,
     minify: 'terser',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
   },
   server: {
     port: 3000,
-    host: true,
-    strictPort: true,
+    host: true
   }
 });
